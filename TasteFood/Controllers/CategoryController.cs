3adaed539f1f15ce34ebbcee2032a679a45b86a0
AK <ali.kuru6@hotmail.com>
2024-02-27 22:09:36 +0300

    Add Controller and Fix Front End

---
+++ TasteFood/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFood.Context;
+using TasteFood.Entites;
+
+namespace TasteFood.Controllers
+{
+    public class CategoryController : Controller
+    {
+        TasteContext context = new TasteContext();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryList()
+        {
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var values = context.Categories.Find(id);
+            context.Categories.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value=context.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = context.Categories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            context.SaveChanges();
+
+            return RedirectToAction("CategoryList");
+           
+        }
+
+
+    }
+}
